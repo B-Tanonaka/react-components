34b93f41c9bff44248ca20569b5115d1416ecbc5
Brett Tanonaka <tano.brett@gmail.com>
2023-02-21 11:41:49 -0800

    Created a grocery list that crosses out when clicked

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+
+  const [purchased, canPurchase] = useState(false);
+
+  const style = {
+    textDecoration: purchased ? 'line-through' : 'none',
+    color: purchased ? 'rebeccapurple' : 'black',
+    fontFamily: 'Helvetica, sans-serif',
+    fontSize: purchased ? '50px' : '18px',
+  };
+
+  return (
+    <li style={style} onClick={() => canPurchase(!purchased)}>{props.item}
+    </li>
+  );
+}
+
+const GroceryList = (props) => (
+  <ul>
+    {props.groceryListItems.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+);
+
+const App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceryListItems={['Eggs', 'Oat milk', 'Cheese', 'Ant trap', 'Inflatable kiddie pool']} />
+  </div>
+  );
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
